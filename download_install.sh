aa9be76194db81ed7d643d097ee3943a51d77790
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2023-06-18 16:10:45 +0330

    Update download_install.sh

--- download_install.sh
+++ download_install.sh
@@ -8,19 +8,12 @@ if [ "$(id -u)" -ne 0 ]; then
         echo 'This script must be run by root' >&2
         exit 1
 fi
-
-echo "we are going to download needed files:)"
-GITHUB_REPOSITORY=hiddify-config
-GITHUB_USER=hiddify
-GITHUB_BRANCH_OR_TAG=v9.4.3
-
-# if [ ! -d "/opt/$GITHUB_REPOSITORY" ];then
         apt update
         apt upgrade -y
         apt install -y wget python3-pip dialog unzip
         pip3 install lastversion "requests<=2.29.0"
-        mkdir -p /opt/$GITHUB_REPOSITORY
-        cd /opt/$GITHUB_REPOSITORY
+        mkdir -p /opt/$hiddify-config
+        cd /opt/$hiddify-config
         wget https://github.com/hiddify/hiddify-config/releases/download/v9.4.3/hiddify-config.zip
         unzip -o hiddify-config.zip
         rm hiddify-config.zip
@@ -34,5 +27,5 @@ echo "/opt/hiddify-config/menu.sh">>~/.bashrc
 echo "cd /opt/hiddify-config/">>~/.bashrc
 
 read -p "Press any key to go  to menu" -n 1 key
-cd /opt/$GITHUB_REPOSITORY
+cd /opt/$hiddify-config
 bash menu.sh
